d4ea29d40122c7d7dcbd48e4ec9702f92265d457
ponchix <ponchorc28@gmail.com>
2020-02-24 17:19:04 -0600

    commit

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -6,7 +6,7 @@ export const HEROES: Hero[] = [
 { id: 14, name: 'Spiderman' },
 { id: 15, name: 'Ironman' },
 { id: 16, name: 'Black widow' },
-{ id: 17, name: 'Black panther' },
+{ id: 17, name: 'Batman' },
 { id: 18, name: 'Hawkeye' },
 { id: 19, name: 'Vision' },
 { id: 20, name: 'Capitan America' }
